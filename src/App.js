3bc117e61912f023a461f6ba3ba74f92fa5fb34c
Bashar-Owainat <owainatbashar13@gmail.com>
2021-09-20 07:01:37 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state ={
+      cityName:'',
+      locationResult: {},
+      showLocInfo: false
+    }
+  }
+
+  getLocation = async (e) => {
+    e.preventDefault();
+    
+    await this.setState({
+      cityName : e.target.city.value
+    })
+
+    let reqURl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.cityName}&format=json`;
+
+    let locResult = await axios.get(reqURl);
+    
+
+    this.setState({
+      locationResult: locResult.data[0],
+      showLocInfo: true
+    })
+  }
+
+
+  render() {
+    return(
+        <div>
+
+            <form onSubmit={this.getLocation}>
+              <input type="text" name='city' />
+              <input type="submit" value='Explore!' />
+            </form>
+
+          
+
+          {this.state.showLocInfo &&
+            <>
+              <p>City Name: {this.state.cityName}</p>
+              <p>latitude: {this.state.locationResult.lat}</p>
+              <p>longitude: {this.state.locationResult.lon}</p>
+
+              <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="map" />
+
+            
+            </>
+
+          }
+
+       </div>
+    )
+  }
 }
 
 export default App;
